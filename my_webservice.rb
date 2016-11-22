1e3521fb0f21f529963b7f2f7a1bce80ffa17773
Rob Westwood <rob.westwood@catalystcomputing.co.uk>
2016-11-22 11:01:15 +0000

    Only greet known persons
    Change to PersonResource so that we only return a greeting for people in the
    organisation ('peter' and 'rob'); a 404 is returned to the client for unknown
    persons.

--- my_webservice.rb
+++ my_webservice.rb
@@ -4,6 +4,15 @@ require 'webmachine'
 
 # Define a resource which we will bind to an endpoint
 class PersonResource < Webmachine::Resource
+  def resource_exists?
+    case request.path_info[:person_name]    # Extract person_name from URI
+    when 'peter', 'rob'                     # The people in the organisation
+      true                                  # Resource exists - continue processing
+    else
+      false                                 # Resource doesn't exist - return 404 to client
+    end
+  end
+
   def to_html
    '<html><head></head><body>Hello, person!</body></html>'
   end
